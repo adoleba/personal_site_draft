96e52cc8f4b8c7144ebfe4f412f76b6ba865d64f
adoleba <ania.doleba@wp.pl>
2019-10-25 15:08:41 +0200

    config media and static path

--- personal_site/settings.py
+++ personal_site/settings.py
@@ -118,3 +118,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = ('static',)
+MEDIA_ROOT = ('media',)
+MEDIA_URL = '/media/'
