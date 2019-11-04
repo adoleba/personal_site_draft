ffd0c41e726410f226dae701249827446f2a0027
adoleba <ania.doleba@wp.pl>
2019-11-04 17:20:17 +0100

    thank_you template and view

--- contact/urls.py
+++ contact/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from contact.views import ContactView
+from contact.views import ContactView, ThankYouView
 
+app_name = 'contact'
 
 urlpatterns = [
-    path('', ContactView.as_view(), name='contact')
+    path('', ContactView.as_view(), name='contact'),
+    path('thank-you/', ThankYouView.as_view(), name='thank_you')
 ]
--- contact/views.py
+++ contact/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import redirect
-from django.views.generic import FormView
+from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import FormView, DetailView
+from django.views.generic.base import View
 
 from contact.forms import ContactForm
 
@@ -7,3 +9,10 @@ from contact.forms import ContactForm
 class ContactView(FormView):
     form_class = ContactForm
     template_name = 'contact/contact.html'
+    success_url = reverse_lazy('contact:thank_you')
+
+
+class ThankYouView(View):
+
+    def get(self, request):
+        return render(request, 'contact/thank_you.html')
