f161b8323866288abc42f666e41c903d3320e127
adoleba <ania.doleba@wp.pl>
2019-11-14 17:38:24 +0100

    add jquery to responsive menu

--- static/js/main.js
+++ static/js/main.js
@@ -9,12 +9,3 @@ function changeLanguage() {
     languagesList.style.display = "none";
     }, 3000);
 }
-
-function responsiveMenu() {
-  const menu = document.getElementById("navbarNavDropdown");
-  if (menu.style.display === "block") {
-    menu.style.display = "none";
-  } else {
-    menu.style.display = "block";
-  }
-}
