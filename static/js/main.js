1a584c08cfcee58ff58ab2bf0d6ee9403eccdceb
adoleba <ania.doleba@wp.pl>
2019-11-14 11:09:51 +0100

    onclick instead of mouseout/over for change_language, aligh toogle menu to right

--- static/js/main.js
+++ static/js/main.js
@@ -1,18 +1,17 @@
 function changeLanguage() {
   const languagesList = document.getElementById("languages");
-  if (window.innerWidth > 268) {
-      if (languagesList.style.display === "none") {
-          languagesList.style.display = "block";
-      }
+  if (languagesList.style.display === "none") {
+    languagesList.style.display = "block";
+  } else {
+    languagesList.style.display = "none";
   }
 }
 
-function changeLanguageOut() {
-  const languagesList = document.getElementById("languages");
-  if (window.innerWidth > 268) {
-      if (languagesList.style.display === "block") {
-          languagesList.style.display = "none";
-      }
+function responsiveMenu() {
+  const menu = document.getElementById("navbarNavDropdown");
+  if (menu.style.display === "block") {
+    menu.style.display = "none";
+  } else {
+    menu.style.display = "block";
   }
 }
-
