2eba731cbc19fd6b681665a964ef1355c62c67ec
adoleba <ania.doleba@wp.pl>
2021-02-04 16:27:22 +0100

    remove empty files and unused imports

--- contact/urls.py
+++ contact/urls.py
@@ -1,4 +1,3 @@
-from django.conf.urls.i18n import i18n_patterns
 from django.urls import path
 from django.utils.translation import gettext_lazy as _
 
--- projects/urls.py
+++ projects/urls.py
@@ -1,5 +1,3 @@
-from django.conf.urls.i18n import i18n_patterns
-from django.utils.translation import gettext_lazy as _
 from django.urls import path
 from projects.views import projects
 
